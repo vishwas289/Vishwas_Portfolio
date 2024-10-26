eff2505c04cb13d9b97859a03c8c92efbcdb3132
vishwas289 <vishwasjadhav289@gmail.com>
2024-10-27 01:00:48 +0530

    Changes
    Site Updated

--- data.js
+++ data.js
@@ -8,10 +8,5 @@ const servicesData = [ {
         title: "Backend Development", 
         category: "backend",
         description: ['In my role as a web developer, I have delved into the basics of backend development within the Adobe Experience Manager (AEM). I have successfully crafted basic Sling models, harnessing the power of Java and the Apache Sling framework to efficiently handle data within the AEM ecosystem. ', ' My experience extends to the implementation of services, where I have skillfully orchestrated the interaction between different services, leveraging service ranking to prioritize and manage their execution. Additionally, I have contributed to the configuration of OSGi (Open Service Gateway Initiative), implementing basic OSGi configurations to fine-tune the behavior of AEM components.']
-    },
-    {
-        title: "Fullstack Development", 
-    category: "fullstack",
-    description: ['Fullstack development involves proficiency in both frontend and backend technologies, enabling a developer to work on all aspects of a web application. A fullstack developer possesses skills in languages such as HTML, CSS, and JavaScript for crafting user interfaces and experiences on the frontend, while also being adept in server-side scripting languages like Node.js, Python, or Java for backend development. This holistic expertise allows them to design, implement, and maintain entire web applications, ensuring seamless integration between the user interface and server-side functionalities. Fullstack developers play a pivotal role in creating end-to-end solutions, from conceptualization to deployment, and are well-positioned to address the diverse challenges of modern web development.']
     }
 ]
