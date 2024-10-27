d61cb821b426cbff1f60735a479e63aa96e096bf
vishwas289 <vishwasjadhav289@gmail.com>
2024-10-28 01:23:19 +0530

    Project

---
+++ nav.js
@@ -0,0 +1,31 @@
+//NAV toggle 
+
+const navMenu = document.querySelector('.nav__menu')
+const navOpenBtn = document.querySelector('.nav__toggle-open')
+const navCloseBtn = document.querySelector('.nav__toggle-close')
+
+const openNavHandler = () => {
+    navMenu.style.display = 'flex';
+    navOpenBtn.style.display = 'none';
+    navCloseBtn.style.display = 'inline-block';
+}
+
+const closeNavHandler = () => {
+    navMenu.style.display = 'none';
+    navOpenBtn.style.display = 'inline-block';
+    navCloseBtn.style.display = 'none';
+}
+
+navOpenBtn.addEventListener('click', openNavHandler)
+navCloseBtn.addEventListener('click', closeNavHandler)
+
+//close nav
+
+const navItems = navMenu.querySelectorAll('a');
+
+
+if (window.innerWidth < 768) {
+
+    navItems.forEach(item => { item.addEventListener('click', closeNavHandler) })
+
+}
