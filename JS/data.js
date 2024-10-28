0c4d77ceaaf8722c32ec1b74a805962a4b48aa3e
vishwas289 <vishwasjadhav289@gmail.com>
2024-10-28 19:16:57 +0530

    changes

---
+++ JS/data.js
@@ -0,0 +1,12 @@
+const servicesData = [ {
+    
+    title: "Frontend Development", 
+    category: "frontend",
+    description: ['In my professional journey as a front-end web developer, I have cultivated expertise in a diverse set of technologies, including HTML, CSS, JavaScript, SCSS, and Bootstrap. My proficiency extends beyond mere coding, as I have successfully designed and implemented authorable websites using Adobe Experience Manager (AEM).', 'Within the AEM framework, I leveraged my skills to create dynamic and visually appealing websites, employing a variety of CSS techniques and adhering to specific policies. This involved not only ensuring the seamless integration of design elements but also optimizing the user experience by implementing responsive and accessible web solutions.']
+    },
+    {
+        title: "Backend Development", 
+        category: "backend",
+        description: ['In my role as a web developer, I have delved into the basics of backend development within the Adobe Experience Manager (AEM). I have successfully crafted basic Sling models, harnessing the power of Java and the Apache Sling framework to efficiently handle data within the AEM ecosystem. ', ' My experience extends to the implementation of services, where I have skillfully orchestrated the interaction between different services, leveraging service ranking to prioritize and manage their execution. Additionally, I have contributed to the configuration of OSGi (Open Service Gateway Initiative), implementing basic OSGi configurations to fine-tune the behavior of AEM components.']
+    }
+]
